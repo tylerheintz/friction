9bc7c7e48818fe17d34576e846561a5685509b9f
tylerheintz <tyler@heintznet.com>
2015-02-19 22:56:25 +0000

    capitalization

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   
   validates_uniqueness_of :phone_number
+  
+  def first_name=(s)
+    write_attribute(:first_name, s.to_s.titleize) 
+  end
+  
+  def last_name=(t)
+    write_attribute(:last_name, t.to_s.titleize)
+  end
+  
 end
